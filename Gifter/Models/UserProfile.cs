fd0963c662b1b01745c769ac6c2789e546a670b0
Rev Huffman <148085981+Revvvolution@users.noreply.github.com>
2024-09-09 10:57:41 -0400

    Added model class for Comment, Post, and UserProfile

---
+++ Gifter/Models/UserProfile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+
+namespace Gifter.Models
+{
+    public class UserProfile
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        [Required]
+        public DateTime DateCreated { get; set; }
+    }
+}
