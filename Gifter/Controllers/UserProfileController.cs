4f7f05f701f708183d08245419918ccc20ca3741
Rev Huffman <148085981+Revvvolution@users.noreply.github.com>
2024-09-10 16:24:12 -0400

    Completed creation, implementation, and testing for CRUD functions for User Profiles

---
+++ Gifter/Controllers/UserProfileController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Gifter.Repositories;
+using Gifter.Models;
+
+namespace Gifter.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserProfileController : ControllerBase
+    {
+        private readonly IUserProfileRepository _userProfileRepository;
+        public UserProfileController(IUserProfileRepository userProfileRepository)
+        {
+            _userProfileRepository = userProfileRepository;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_userProfileRepository.GetAll());
+        }
+
+        [HttpGet("GetAllWithPosts")]
+        public IActionResult GetAllWithPosts()
+        {
+            var userProfiles = _userProfileRepository.GetAllWithPosts();
+            return Ok(userProfiles);
+        }
+
+        [HttpGet("GetUserProfile/{id}")]
+        public IActionResult GetUserProfile(int id)
+        {
+            var userProfile = _userProfileRepository.GetById(id);
+            if (userProfile == null)
+            {
+                return NotFound();
+            }
+            return Ok(userProfile);
+        }
+
+        [HttpGet("GetUserProfileByIdWithPosts/{id}")]
+        public IActionResult GetUserProfileByIdWithPosts(int id)
+        {
+            var userProfile = _userProfileRepository.GetUserProfileByIdWithPosts(id);
+            if (userProfile == null)
+            {
+                return NotFound();
+            }
+            return Ok(userProfile);
+        }
+
+        [HttpPost]
+        public IActionResult UserProfile(UserProfile userProfile)
+        {
+            _userProfileRepository.Add(userProfile);
+            return CreatedAtAction("Get", new { id = userProfile.Id }, userProfile);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, UserProfile userProfile)
+        {
+            if (id != userProfile.Id)
+            {
+                return BadRequest();
+            }
+
+            _userProfileRepository.Update(userProfile);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _userProfileRepository.Delete(id);
+            return NoContent();
+        }
+    }
+}
+
---
+++ Gifter/Repositories/IUserProfileRepository.cs
@@ -0,0 +1,15 @@
+﻿using Gifter.Models;
+
+namespace Gifter.Repositories
+{
+    public interface IUserProfileRepository
+    {
+        void Add(UserProfile userProfile);
+        void Delete(int id);
+        List<UserProfile> GetAll();
+        List<UserProfile> GetAllWithPosts();
+        UserProfile GetById(int id);
+        UserProfile GetUserProfileByIdWithPosts(int id);
+        void Update(UserProfile userProfile);
+    }
+}
