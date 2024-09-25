a84c59ffd425416b5e5325e80ef8cde47108ed95
Rev Huffman <148085981+Revvvolution@users.noreply.github.com>
2024-09-25 16:15:15 -0400

    Added user profile controller testing for GetAll method and passed initial testing.

---
+++ Gifter.Tests/Mocks/InMemoryUserProfileRepository.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Gifter.Models;
+using Gifter.Repositories;
+
+namespace Gifter.Tests.Mocks
+{
+    class InMemoryUserProfileRepository : IUserProfileRepository
+    {
+        private readonly List<UserProfile> _data;
+
+        public List<UserProfile> InternalData
+        {
+            get
+            {
+                return _data;
+            }
+        }
+
+        public InMemoryUserProfileRepository(List<UserProfile> startingData)
+        {
+            _data = startingData;
+        }
+
+        public void Add(UserProfile userProfile)
+        {
+            var lastUserProfile = _data.Last();
+            userProfile.Id = lastUserProfile.Id + 1;
+            _data.Add(userProfile);
+        }
+
+        public void Delete(int id)
+        {
+            var userProfileTodelete = _data.FirstOrDefault(p => p.Id == id);
+            if (userProfileTodelete == null)
+            {
+                return;
+            }
+
+            _data.Remove(userProfileTodelete);
+        }
+
+        public List<UserProfile> GetAll()
+        {
+            return _data;
+        }
+
+        public List<UserProfile> GetAllWithPosts()
+        {
+            return _data;
+        }
+
+        public UserProfile GetById(int id)
+        {
+            return _data.FirstOrDefault(p => p.Id == id);
+        }
+
+        public UserProfile GetUserProfileByIdWithPosts(int id)
+        {
+            return _data.FirstOrDefault(p => p.Id == id);
+        }
+
+        public void Update(UserProfile userProfile)
+        {
+            var currentUserProfile = _data.FirstOrDefault(p => p.Id == userProfile.Id);
+            if (currentUserProfile == null)
+            {
+                return;
+            }
+
+            currentUserProfile.Name = userProfile.Name;
+            currentUserProfile.Email = userProfile.Email;
+            currentUserProfile.ImageUrl = userProfile.ImageUrl;
+            currentUserProfile.DateCreated = userProfile.DateCreated;
+        }
+
+        public List<UserProfile> Search(string criterion, bool sortDescending)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<UserProfile> GetAllWithComments()
+        {
+            throw new NotImplementedException();
+        }
+
+        public UserProfile GetUserProfileByIdWithComments(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+    }
+}
---
+++ Gifter.Tests/UserProfileControllerTests.cs
@@ -0,0 +1,68 @@
+﻿using Gifter.Controllers;
+using Gifter.Models;
+using Gifter.Tests.Mocks;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Gifter.Tests
+{
+    public class UserProfileControllerTests
+    {
+
+        [Fact]
+        public void Get_Returns_All_UserProfiles()
+        {
+            // Arrange 
+            var userProfileCount = 20;
+            var userProfiles = CreateTestUserProfiles(userProfileCount);
+
+            var repo = new InMemoryUserProfileRepository(userProfiles);
+            var controller = new UserProfileController(repo);
+
+            // Act 
+            var result = controller.Get();
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var actualUserProfiles = Assert.IsType<List<UserProfile>>(okResult.Value);
+
+            Assert.Equal(userProfileCount, actualUserProfiles.Count);
+            Assert.Equal(userProfiles, actualUserProfiles);
+        }
+
+
+        private List<UserProfile> CreateTestUserProfiles(int count)
+        {
+            var userProfiles = new List<UserProfile>();
+            for (var i = 1; i <= count; i++)
+            {
+                userProfiles.Add(new UserProfile()
+                {
+                    Id = i,
+                    Name = $"User {i}",
+                    Email = $"user{i}@example.com",
+                    DateCreated = DateTime.Today.AddDays(-i),
+                    ImageUrl = $"http://user.image.url/{i}",
+                });
+            }
+            return userProfiles;
+        }
+
+        private UserProfile CreateTestUserProfile(int id)
+        {
+            return new UserProfile()
+            {
+                Id = id,
+                Name = $"User {id}",
+                Email = $"user{id}@example.com",
+                DateCreated = DateTime.Today.AddDays(-id),
+                ImageUrl = $"http://user.image.url/{id}",
+            };
+        }
+
+
+    }
+}
